9fa6bbffa2b619dfc6976214de0c85690e0411f8
Sanjay Kumar <68441565+sanjay-kumar97@users.noreply.github.com>
2023-12-06 09:44:03 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -5,6 +5,6 @@ const dbPassword = "dbPassword";
 const dbName = "mern-demo.3o05jpn.mongodb.net";
 const params = "retryWrites=true&w=majority";
 
-const mongoURL = `mongodb+srv://${dbUsername}:${dbPassword}@${dbName}/test/?${params}`;
+const mongoURL = `mongodb+srv://${dbUsername}:${dbPassword}@${dbName}/test?${params}`;
 
 module.exports = { PORT, mongoURL };
