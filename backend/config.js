fb1f050ca12627a935d335d90a4c38eda08c70a7
sanjay-kumar97 <sanjaykumar2ygl@gmail.com>
2023-12-11 01:08:11 +0530

    Fix: CORS url format change

--- backend/config.js
+++ backend/config.js
@@ -8,8 +8,8 @@ const params = "retryWrites=true&w=majority";
 const mongoURL = `mongodb+srv://${dbUsername}:${dbPassword}@${dbName}/test?${params}`;
 
 const allowedOrigins = [
-  "http://localhost:3000/",
-  "https://mern-demo-frontend.vercel.app/",
+  "http://localhost:3000",
+  "https://mern-demo-frontend.vercel.app",
 ];
 
 const allowedMethods = ["POST", "GET", "PUT", "DELETE"];
